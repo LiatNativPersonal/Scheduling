6b3c0a89a6555cb1636fab4c97c0039e7de30c56
LiatNativPersonal <liatnativ@gmail.com>
2020-03-26 16:41:20 +0200

    Added index attribute

--- Job.py
+++ Job.py
@@ -9,12 +9,13 @@ from JobType import JobType
 
 class Job:
     
-    def __init__(self, job_type, processing_time):
+    def __init__(self, job_type, processing_time, index):
         self.job_type = job_type
         self.proc_time = processing_time
+        self.index = index
         
     def printJob(self):
-        print("Job Type: {}, processing time: {} \n".format((self.job_type).name), self.proc_time)
+        print("Job {}: Job Type= {}, processing time= {} \n".format(self.index, (self.job_type).name), self.proc_time)
         
     
         
